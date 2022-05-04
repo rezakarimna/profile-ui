77aeeaa4ed9dacfd753d3fe73cdda618ccb1d452
Reza <karimnia.reza70@yahoo.com>
2022-05-04 19:19:51 +0430

    add icon heart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -25,16 +24,17 @@ class MyApp extends StatelessWidget {
           // Notice that the counter didn't reset back to zero; the application
           // is not restarted.
           primarySwatch: Colors.blue,
+          primaryColor: Colors.pink.shade400,
           brightness: Brightness.dark,
           scaffoldBackgroundColor: Color.fromARGB(255, 30, 30, 30),
           appBarTheme: AppBarTheme(backgroundColor: Colors.black),
           fontFamily: 'primary',
           textTheme: const TextTheme(
-            bodyText1: TextStyle(fontSize: 13),
+              bodyText1: TextStyle(
+                  fontSize: 13, color: Color.fromARGB(200, 255, 255, 255)),
               bodyText2: TextStyle(fontSize: 15),
-              headline6: TextStyle(fontWeight: FontWeight.bold)
-          )
-          ),
+              headline6: TextStyle(fontWeight: FontWeight.bold),
+              subtitle1: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
       home: MyHomePage(),
     );
   }
@@ -67,33 +67,56 @@ class MyHomePage extends StatelessWidget {
                   const SizedBox(
                     width: 16,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Brice seraphin'),
-                      SizedBox(
-                        height: 2,
-                      ),
-                      Text('Product & print'),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Row(
-                        children: [
-                          Icon(CupertinoIcons.location),
-                          Text('Paris , France'),
-                        ],
-                      )
-                    ],
-                  )
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Brice seraphin',
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                        SizedBox(
+                          height: 2,
+                        ),
+                        Text('Product & print'),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.location,
+                              size: 14,
+                              color:
+                                  Theme.of(context).textTheme.bodyText1!.color,
+                            ),
+                            SizedBox(
+                              width: 3,
+                            ),
+                            Text(
+                              'Paris , France',
+                              style: Theme.of(context).textTheme.caption,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Icon(
+                    CupertinoIcons.heart,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ],
               ),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(32, 0, 32, 16),
               child: Text(
-                  'Short monologues are great for acting tests You, like most actors and actresses, have probably searched words like "monologue for acting test'),
-            )
+                'Short monologues are great for acting tests You, like most actors and actresses, have probably searched words like "monologue for acting test',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+            ),
+            Divider()
           ],
         ));
   }
