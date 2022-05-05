e9394298b1c6687e9caf4b113afb7fd2fc180b98
Reza <karimnia.reza70@yahoo.com>
2022-05-05 21:26:16 +0430

    Add Skill

--- lib/main.dart
+++ lib/main.dart
@@ -40,9 +40,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+enum _SkillType { photoshop, xd, illustrator, afterEffects, lightRoom }
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _SkillType _skill = _SkillType.photoshop;
+
+  void updateSelectSkill(_SkillType skillType) {
+    setState(() {
+      this._skill = skillType;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var data = 'Photoshop';
     return Scaffold(
         appBar: AppBar(title: const Text("Curriculum vitae"), actions: const [
           Icon(CupertinoIcons.chat_bubble),
@@ -52,6 +68,7 @@ class MyHomePage extends StatelessWidget {
           )
         ]),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.all(20),
@@ -116,8 +133,146 @@ class MyHomePage extends StatelessWidget {
                 style: Theme.of(context).textTheme.bodyText1,
               ),
             ),
-            Divider()
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(32, 16, 32, 12),
+              child: Row(
+                children: [
+                  Text(
+                    "Skils",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText2!
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Icon(
+                    CupertinoIcons.chevron_down,
+                    size: 12,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Center(
+              child: Wrap(
+                direction: Axis.horizontal,
+                spacing: 8,
+                runSpacing: 8,
+                children: [
+                  Skill(
+                    type: _SkillType.photoshop,
+                    title: 'Photoshop',
+                    imagePath: 'assets/images/photoshop.png',
+                    shadowColor: Colors.blue,
+                    isActive: _skill == _SkillType.photoshop,
+                    onTop: () {
+                      updateSelectSkill(_SkillType.photoshop);
+                    },
+                  ),
+                  Skill(
+                      type: _SkillType.xd,
+                      title: 'Adobe XD',
+                      imagePath: 'assets/images/adobe_xd.png',
+                      shadowColor: Colors.pink,
+                      isActive: _skill == _SkillType.xd,
+                      onTop: () {
+                        updateSelectSkill(_SkillType.xd);
+                      }),
+                  Skill(
+                    type: _SkillType.illustrator,
+                    title: 'Illustrator',
+                    imagePath: 'assets/images/illustrator.png',
+                    shadowColor: Colors.orange,
+                    isActive: _skill == _SkillType.illustrator,
+                    onTop: () {
+                      updateSelectSkill(_SkillType.illustrator);
+                    },
+                  ),
+                  Skill(
+                      type: _SkillType.afterEffects,
+                      title: 'After Effects',
+                      imagePath: 'assets/images/after_effects.png',
+                      shadowColor: Colors.blue.shade800,
+                      isActive: _skill == _SkillType.afterEffects,
+                      onTop: () {
+                        updateSelectSkill(_SkillType.afterEffects);
+                      }),
+                  Skill(
+                      type: _SkillType.lightRoom,
+                      title: 'Lightroom',
+                      imagePath: 'assets/images/lightroom.png',
+                      shadowColor: Colors.blue,
+                      isActive: _skill == _SkillType.lightRoom,
+                      onTop: () {
+                        updateSelectSkill(_SkillType.lightRoom);
+                      }),
+                ],
+              ),
+            )
           ],
         ));
+  }
+}
+
+class Skill extends StatelessWidget {
+  final _SkillType type;
+  final String title;
+  final String imagePath;
+  final Color shadowColor;
+  final bool isActive;
+  final Function() onTop;
+
+  const Skill(
+      {Key? key,
+      required this.type,
+      required this.title,
+      required this.imagePath,
+      required this.shadowColor,
+      required this.isActive,
+      required this.onTop})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(12),
+      onTap: onTop,
+      child: Container(
+        width: 110,
+        height: 110,
+        decoration: isActive
+            ? BoxDecoration(
+                color: Color.fromARGB(25, 255, 255, 255),
+                borderRadius: BorderRadius.circular(12))
+            : null,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: isActive
+                  ? BoxDecoration(boxShadow: [
+                      BoxShadow(
+                          color: shadowColor.withOpacity(0.5), blurRadius: 20)
+                    ])
+                  : null,
+              child: Image.asset(
+                imagePath,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Text(title)
+          ],
+        ),
+      ),
+    );
   }
 }
